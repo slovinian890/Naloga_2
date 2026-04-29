adaaa86527e1dbb46007e147e8846863544ccfdc
Filip <sinjurfilip@gmail.com>
2026-04-29 10:28:52 +0200

    backend tapravi

--- backend/result_2_backend/src/main/java/com/result/result_naloga_2/ResultNaloga2Application.java
+++ backend/result_2_backend/src/main/java/com/result/result_naloga_2/ResultNaloga2Application.java
@@ -1,13 +1,13 @@
-package com.example.server;
+package com.result.result_naloga_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ResultNaloga2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(ResultNaloga2Application.class, args);
 	}
 
 }
--- backend/result_2_backend/src/test/java/com/result/result_naloga_2/ResultNaloga2ApplicationTests.java
+++ backend/result_2_backend/src/test/java/com/result/result_naloga_2/ResultNaloga2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.server;
+package com.result.result_naloga_2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class ResultNaloga2ApplicationTests {
 
 	@Test
 	void contextLoads() {
